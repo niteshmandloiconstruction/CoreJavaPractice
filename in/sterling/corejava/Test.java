03f8a87615f5fe6dc4a5aa323d9863dc5b66a100
niteshmandloiconstruction <104087381+niteshmandloiconstruction@users.noreply.github.com>
2022-04-21 22:35:37 +0530

    Update Test.java

--- in/sterling/corejava/Test.java
+++ in/sterling/corejava/Test.java
@@ -3,18 +3,7 @@ package in.sterling.corejava;
 public class Test {
 
 	
-	public static Object main(String[] args) {
-		
-		class Demo{
-			
-			public void add() {
-				System.out.println("Hello");
-			}
-		
-	}
-		
-	return new Demo(); 
-	}
+	
 	
 	
 
