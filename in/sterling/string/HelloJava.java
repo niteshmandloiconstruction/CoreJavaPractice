f0364a29edbd8d9a89f0c6a73aaac32ad0d27d91
Nitesh <niteshmandloiconstruction@gmail.com>
2022-04-22 11:49:08 +0530

    Called secondMethod

--- in/sterling/string/HelloJava.java
+++ in/sterling/string/HelloJava.java
@@ -1,13 +1,14 @@
 public class HelloJava{
 public static void main(String[] args){
 getPost();
+secondMethod();
 }
 
 public static void getPost(){
 System.out.println("Hii i am Nitesh");
 }
 
-public void secondMethod() {
+public static void secondMethod() {
 	System.out.println("Hello");
 	}
 }
