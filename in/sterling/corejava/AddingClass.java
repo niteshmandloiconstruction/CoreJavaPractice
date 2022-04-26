b4eefad600dc402388a57a8a90713cb91c19f53f
Nitesh <niteshmandloiconstruction@gmail.com>
2022-04-26 16:42:34 +0530

    Added class

---
+++ in/sterling/corejava/AddingClass.java
@@ -0,0 +1,11 @@
+package in.sterling.corejava;
+
+public class AddingClass {
+
+@Override
+protected Object clone() throws CloneNotSupportedException {
+	// TODO Auto-generated method stub
+	return super.clone();
+}	
+	
+}
